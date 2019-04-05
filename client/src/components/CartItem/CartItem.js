9505218a460f7d4ae43acf51e8bafa76442c8273
Edwin Rivera <waehake@gmail.com>
2019-04-05 11:01:07 -0400

    changed the cart item font color, fixed an issue with incrementing and decrementing items inside the cartitem component.

--- client/src/components/CartItem/CartItem.js
+++ client/src/components/CartItem/CartItem.js
@@ -9,22 +9,25 @@ const CartItem = ({ value, item }) => {
       <h3 className="cartItemName">{item.title}</h3>
       <h3 className="cartItemPrice">{item.price}</h3>
       <div className="cartItemQuantity">
-        <button className="blackBtn" onClick={() => value.decrement(item.id)}>
+        <button className="blackBtn" onClick={() => value.decrement(item._id)}>
           {/* minus html hex code */}
           &#8722;
         </button>
         <button className="blackBtnNum">{item.count}</button>
-        <button className="blackBtn" onClick={() => value.increment(item.id)}>
+        <button className="blackBtn" onClick={() => value.increment(item._id)}>
           {/* plus html hex code */}
           &#x2b;
         </button>
       </div>
       <div className="cartItemRemove">
-        <button className="removeBtn" onClick={() => value.removeItem(item.id)}>
+        <button
+          className="removeBtn"
+          onClick={() => value.removeItem(item._id)}
+        >
           <i className="fas fa-trash-alt cartIcon" />
         </button>
       </div>
-      <h3 className="cartItemTotal">Quantity Total</h3>
+      <h3 className="cartItemTotal">Quantity Total {item.count}</h3>
     </div>
   );
 };
